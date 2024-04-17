2f06ba25931c0e93dc5b3e0f22584f045604f1ab
razvanmatisan <razvan.matisan@student.uva.nl>
2024-04-18 01:39:02 +0200

    Add trainer class for the Pretraining Model

---
+++ main.py
@@ -0,0 +1,58 @@
+from dataset import get_fashion_mnist_dataloader
+from trainer import TrainerAutoregressor, TrainerClassifier
+
+
+def train_autoregressor():
+    train_dataloader = get_fashion_mnist_dataloader(
+        pretraining=True, train=True, batch_size=1024
+    )
+
+    val_dataloader = get_fashion_mnist_dataloader(
+        pretraining=True, train=False, batch_size=1024
+    )
+
+    trainer = TrainerAutoregressor(
+        dummy_imgs=next(iter(train_dataloader))[0],
+        norm_pix_loss=True,
+        patch_size=196,
+        num_layers=8,
+        num_heads=6,
+        embedding_dimension=768,
+        hidden_dimension=128,
+        dropout_probability=0.1,
+    )
+
+    trainer.train_model(train_dataloader, val_dataloader, num_epochs=10)
+    val_mse = trainer.eval_model(val_dataloader)
+    print(val_mse)
+
+
+def train_classifier():
+    train_dataloader = get_fashion_mnist_dataloader(
+        pretraining=False, train=True, batch_size=1024
+    )
+
+    val_dataloader = get_fashion_mnist_dataloader(
+        pretraining=False, train=False, batch_size=1024
+    )
+
+    trainer = TrainerClassifier(
+        dummy_imgs=next(iter(train_dataloader))[0],
+        num_categories=10,
+        num_layers=8,
+        num_heads=4,
+        embedding_dimension=768,
+        hidden_dimension=128,
+        dropout_probability=0.1,
+    )
+
+    trainer.train_model(train_dataloader, val_dataloader, num_epochs=10)
+    val_acc = trainer.eval_model(val_dataloader)
+    print(val_acc)
+
+
+# TODO: Argparser
+# TODO: Making the methods more generic (adding some arguments for the two functions lol)
+if __name__ == "__main__":
+    train_classifier()
+    train_autoregressor()
--- trainer.py
+++ trainer.py
@@ -9,15 +9,153 @@ from flax.training import train_state, checkpoints
 import optax
 import jax
 
+import jax.numpy as jnp
 from jax import random
-from model import ClassificationModel
-from dataset import get_fashion_mnist_dataloader
-
+from model import ClassificationModel, PretrainingModel
 
 # TODO: Adding lr scheduler / weight decay?
 # TODO: Logging?
 # TODO: Train/val/test split. Currently, there're only train and validation dataloaders
-# TODO: Add an argparser
+
+# TODO: Implementing a generic Trainer class
+
+
+class TrainerAutoregressor:
+    def __init__(
+        self, dummy_imgs, lr=1e-3, seed=42, norm_pix_loss=False, **model_hparams
+    ):
+        super().__init__()
+        self.lr = lr
+        self.seed = seed
+        self.rng = jax.random.PRNGKey(self.seed)
+        self.norm_pix_loss = norm_pix_loss
+
+        self.log_dir = os.path.join(str(Path.cwd()), "checkpoints/autoregressor/")
+
+        # Empty model
+        self.model = PretrainingModel(**model_hparams)
+
+        # Initialize model
+        self.init_model(dummy_imgs)
+
+        # Jitting train and eval steps
+        self.train_step = jax.jit(self.train_step)
+        self.eval_step = jax.jit(self.eval_step)
+
+    def init_model(self, exmp_imgs):
+        self.rng, init_rng, dropout_init_rng = random.split(self.rng, 3)
+        self.init_params = self.model.init(
+            {"params": init_rng, "dropout": dropout_init_rng}, exmp_imgs, training=True
+        )["params"]
+        self.state = None
+
+    def get_loss(self, params, rng, batch, train):
+        """
+        imgs, targets, mask: [bs, 28, 28]
+        """
+        imgs, targets, mask = batch
+
+        # Normalize the target
+        if self.norm_pix_loss:
+            mean = jnp.mean(targets, axis=(-2, -1), keepdims=True)  # shape [bs, 1, 1]
+            var = jnp.var(targets, axis=(-2, -1), keepdims=True)  # shape [bs, 1, 1]
+            targets = (targets - mean) / (var + 1.0e-6) ** 0.5
+
+        rng, dropout_apply_rng = random.split(rng)
+
+        preds = self.model.apply(
+            {"params": params},
+            imgs,
+            training=train,
+            mask=mask,
+            rngs={"dropout": dropout_apply_rng},
+        )
+
+        # Pixel-wise MSE
+        loss = (preds - targets) ** 2  # shape = [bs, 28, 28]
+        loss = jnp.mean(loss)
+
+        return loss, rng
+
+    def train_step(self, state, rng, batch):
+        def loss_fn(params):
+            return self.get_loss(params, rng, batch, train=True)
+
+        # Get output of loss function and gradients of the loss
+        (loss, rng), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
+        # Update parameters and batch statistics
+        state = state.apply_gradients(grads=grads)
+        return state, rng, loss
+
+    def eval_step(self, state, rng, batch):
+        # Return the mse for a single batch
+        mse, rng = self.get_loss(state.params, rng, batch, train=False)
+        return mse, rng
+
+    def init_optimizer(self):
+        # TODO: lr_scheduler?
+
+        optimizer = optax.chain(
+            optax.clip_by_global_norm(1.0),  # Clip gradients at norm 1
+            optax.adamw(self.lr),
+        )
+        # Initialize training state
+        self.state = train_state.TrainState.create(
+            apply_fn=self.model.apply,
+            params=self.init_params if self.state is None else self.state.params,
+            tx=optimizer,
+        )
+
+    def train_model(self, train_loader, val_loader, num_epochs=200):
+        # Train model for defined number of epochs
+        self.init_optimizer()
+        # Track best eval accuracy
+        best_eval = 999999999
+        for epoch_idx in tqdm(range(1, num_epochs + 1)):
+            self.train_epoch(train_loader, epoch=epoch_idx)
+            eval_mse = self.eval_model(val_loader)
+            if eval_mse <= best_eval:
+                best_eval = eval_mse
+                self.save_model(step=epoch_idx)
+
+    def train_epoch(self, train_loader, epoch):
+        # Train model for one epoch, and print avg loss and accuracy
+        metrics = defaultdict(list)
+        for batch in tqdm(train_loader, desc="Training", leave=False):
+            self.state, self.rng, loss = self.train_step(self.state, self.rng, batch)
+            metrics["mse"].append(loss)
+        for key in metrics:
+            avg_val = np.stack(jax.device_get(metrics[key])).mean()
+            print(f"Epoch {epoch} | Avg_{key} {avg_val}")
+
+    def eval_model(self, data_loader):
+        # Test model on all images of a data loader and return avg accuracy
+        total_mse, count = 0, 0
+        for batch in data_loader:
+            mse, self.rng = self.eval_step(self.state, self.rng, batch)
+            total_mse += mse
+            count += 1
+
+        res = (total_mse / count).item()
+        return res
+
+    def save_model(self, step=0):
+        # Save current model at certain training iteration
+        checkpoints.save_checkpoint(
+            ckpt_dir=self.log_dir, target=self.state.params, step=step, overwrite=True
+        )
+
+    def load_model(self):
+        # Load model
+        params = checkpoints.restore_checkpoint(ckpt_dir=self.log_dir, target=None)
+
+        self.state = train_state.TrainState.create(
+            apply_fn=self.model.apply,
+            params=params,
+            tx=self.state.tx
+            if self.state
+            else optax.adamw(self.lr),  # Default optimizer
+        )
 
 
 class TrainerClassifier:
@@ -61,7 +199,6 @@ class TrainerClassifier:
 
         return loss, (acc, rng)
 
-    # @jax.jit
     def train_step(self, state, rng, batch):
         def loss_fn(params):
             return self.get_loss(params, rng, batch, train=True)
@@ -74,7 +211,6 @@ class TrainerClassifier:
         state = state.apply_gradients(grads=grads)
         return state, rng, loss, acc
 
-    # @jax.jit
     def eval_step(self, state, rng, batch):
         # Return the accuracy for a single batch
         _, (acc, rng) = self.get_loss(state.params, rng, batch, train=False)
@@ -146,25 +282,3 @@ class TrainerClassifier:
             if self.state
             else optax.adamw(self.lr),  # Default optimizer
         )
-
-
-if __name__ == "__main__":
-    train_dataloader = get_fashion_mnist_dataloader(
-        pretraining=False, train=True, batch_size=1024
-    )
-    val_dataloader = get_fashion_mnist_dataloader(
-        pretraining=False, train=False, batch_size=1024
-    )
-
-    trainer = TrainerClassifier(
-        dummy_imgs=next(iter(train_dataloader))[0],
-        num_categories=10,
-        num_layers=8,
-        num_heads=4,
-        embedding_dimension=768,
-        hidden_dimension=128,
-        dropout_probability=0.1,
-    )
-
-    trainer.train_model(train_dataloader, val_dataloader, num_epochs=10)
-    val_acc = trainer.eval_model(val_dataloader)
