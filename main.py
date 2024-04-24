263c9ba7671310b370a3d09a2d4568c46e8bc74c
robert.vanderklis <robert.vanderklis@wur.nl>
2024-04-24 16:13:00 +0200

    Add argparser

--- main.py
+++ main.py
@@ -1,62 +1,131 @@
+import argparse
+
 import jax.numpy as jnp
 
 from dataset import get_fashion_mnist_dataloader
 from trainer import TrainerAutoregressor, TrainerClassifier
 
 
-def train_autoregressor():
+def train_autoregressor(
+    batch_size,
+    num_epochs,
+    embedding_dimension,
+    hidden_dimension,
+    num_layers,
+    num_heads,
+    patch_size,
+):
     train_dataloader = get_fashion_mnist_dataloader(
-        pretraining=True, train=True, batch_size=1024
+        pretraining=True, train=True, batch_size=batch_size
     )
 
     val_dataloader = get_fashion_mnist_dataloader(
-        pretraining=True, train=False, batch_size=1024
+        pretraining=True, train=False, batch_size=batch_size
     )
 
     trainer = TrainerAutoregressor(
         dummy_imgs=next(iter(train_dataloader))[0],
         norm_pix_loss=True,
-        patch_size=196,
+        patch_size=patch_size,
         dtype=jnp.bfloat16,
-        num_layers=8,
-        num_heads=6,
-        embedding_dimension=768,
-        hidden_dimension=128,
+        num_layers=num_layers,
+        num_heads=num_heads,
+        embedding_dimension=embedding_dimension,
+        hidden_dimension=hidden_dimension,
         dropout_probability=0.1,
     )
 
-    trainer.train_model(train_dataloader, val_dataloader, num_epochs=2)
+    trainer.train_model(train_dataloader, val_dataloader, num_epochs=num_epochs)
     val_mse = trainer.eval_model(val_dataloader)
     print(f"Final MSE: {val_mse}")
 
 
-def train_classifier():
+def train_classifier(
+    batch_size,
+    num_epochs,
+    embedding_dimension,
+    hidden_dimension,
+    num_layers,
+    num_heads,
+    num_categories,
+):
     train_dataloader = get_fashion_mnist_dataloader(
-        pretraining=False, train=True, batch_size=1024
+        pretraining=False, train=True, batch_size=batch_size
     )
 
     val_dataloader = get_fashion_mnist_dataloader(
-        pretraining=False, train=False, batch_size=1024
+        pretraining=False, train=False, batch_size=batch_size
     )
 
     trainer = TrainerClassifier(
         dummy_imgs=next(iter(train_dataloader))[0],
         dtype=jnp.bfloat16,
-        num_categories=10,
-        num_layers=8,
-        num_heads=4,
-        embedding_dimension=768,
-        hidden_dimension=128,
+        num_categories=num_categories,
+        num_layers=num_layers,
+        num_heads=num_heads,
+        embedding_dimension=embedding_dimension,
+        hidden_dimension=hidden_dimension,
         dropout_probability=0.1,
     )
 
-    trainer.train_model(train_dataloader, val_dataloader, num_epochs=2)
+    trainer.train_model(train_dataloader, val_dataloader, num_epochs=num_epochs)
     val_acc = trainer.eval_model(val_dataloader)
     print(f"Final accuracy: {val_acc}")
 
 
-# TODO: Argparser
-# TODO: Making the methods more generic (adding some arguments for the two functions lol)
 if __name__ == "__main__":
-    train_classifier()
-    train_autoregressor()
+    parser = argparse.ArgumentParser(description="Train model")
+
+    parser.add_argument("--epochs", type=int, required=True, help="Number of epochs")
+    parser.add_argument(
+        "--train_classifier", action="store_true", help="Train classifier"
+    )
+    parser.add_argument(
+        "--train_autoregressor", action="store_true", help="Train autoregressor"
+    )
+
+    parser.add_argument(
+        "--num_categories",
+        type=int,
+        default=10,
+        help="Number of classification categories",
+    )
+    parser.add_argument("--batch_size", type=int, default=1024, help="Batch size")
+    parser.add_argument(
+        "--embedding_dimension",
+        type=int,
+        default=768,
+        help="Embedding dimension of the tokens",
+    )
+    parser.add_argument(
+        "--hidden_dimension",
+        type=int,
+        default=128,
+        help="Hidden dimension of the model",
+    )
+    parser.add_argument("--num_layers", type=int, default=8, help="Number of layers")
+    parser.add_argument(
+        "--num_heads", type=int, default=6, help="Number of attention heads"
+    )
+    parser.add_argument("--patch_size", type=int, default=196, help="Patch size")
+
+    args = parser.parse_args()
+
+    train_classifier(
+        args.batch_size,
+        args.epochs,
+        args.embedding_dimension,
+        args.hidden_dimension,
+        args.num_layers,
+        args.num_heads,
+        args.num_categories,
+    )
+    train_autoregressor(
+        args.batch_size,
+        args.epochs,
+        args.embedding_dimension,
+        args.hidden_dimension,
+        args.num_layers,
+        args.num_heads,
+        args.patch_size,
+    )
